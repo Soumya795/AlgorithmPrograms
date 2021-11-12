3357817985c8fc9f3cf7eea81149687aff5ace4b
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-12 16:36:31 +0530

    Added:Bubble Sort of String

---
+++ src/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    public static void main(String []args) {
+        String[] str;
+        str = new String[]{ "Cricket", "Football", "Rafting", "VolleyBall", "Archery"};
+        String temp;
+        System.out.println("Bubble Sorted string is in following order :- ");
+        for (int j = 0; j < str.length; j++) {
+            for (int i = j + 1; i < str.length; i++) {
+                if (str[i].compareTo(str[j]) < 0) {
+                    temp = str[j];
+                    str[j] = str[i];
+                    str[i] = temp;
+                }
+            }
+            System.out.println(str[j]);
+        }
+    }
+}
+
